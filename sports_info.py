a0a6d44cebe1478e15e24e6be112f46f68211c8a
matthewhogan22 <matthewhogan2203@gmail.com>
2026-01-30 17:36:28 -0500

    Updated to avoid rate limits

--- sports_info.py
+++ sports_info.py
@@ -13,6 +13,9 @@ nba_dict = {}
 mlb_dict = {}
 ncaaf_dict = {}
 
+# Data Holders for all Teams
+ncaaf_teams_dict = {}
+
 # Base variables needed across the whole code
 
 def get_league_data(sport, league):
@@ -166,6 +169,26 @@ def set_mlb_dict():
 
         mlb_dict[game_name] = game_dict
 
+def set_ncaaf_teams_dict():
+    ncaaf_data = get_league_data("football", "college-football")
+    games = ncaaf_data["events"]
+    for game in games:
+        home_team_raw = game["competitions"][0]["competitors"][0]
+        home_team_id = home_team_raw["id"]
+        home_conf, home_color = get_ncaaf_team_data(home_team_id)
+        home_dict = {}
+        home_dict["conf"] = home_conf
+        home_dict["color"] = home_color
+        ncaaf_teams_dict[home_team_id] = home_dict
+
+        away_team_raw = game["competitions"][0]["competitors"][1]
+        away_team_id = away_team_raw["id"]
+        away_conf, away_color = get_ncaaf_team_data(away_team_id)
+        away_dict = {}
+        away_dict["conf"] = away_conf
+        away_dict["color"] = away_color
+        ncaaf_teams_dict[away_team_id] = away_dict
+
 def get_ncaaf_team_data(team_id):
     r = requests.get(f"https://site.api.espn.com/apis/site/v2/sports/football/college-football/teams/{team_id}")
     team_data = r.json()
@@ -194,18 +217,14 @@ def set_ncaaf_dict():
         # Team Section
         home_team_raw = game["competitions"][0]["competitors"][0]
         home_team_id = home_team_raw["id"]
-        home_conf, home_color = get_ncaaf_team_data(home_team_id)
-        game_dict["home_conf"] = home_conf
-        game_dict["home_color"] = home_color
+        game_dict["home_id"] = home_team_id
         home_team_name = home_team_raw["team"]["abbreviation"]
         home_score = game["competitions"][0]["competitors"][0]["score"]
         game_dict["home_score"] = home_score
 
         away_team_raw = game["competitions"][0]["competitors"][1]
         away_team_id = away_team_raw["id"]
-        away_conf, away_color = get_ncaaf_team_data(away_team_id)
-        game_dict["away_conf"] = away_conf
-        game_dict["away_color"] = away_color
+        game_dict["away_id"] = away_team_id
         away_team_name = away_team_raw["team"]["abbreviation"]
         away_score = game["competitions"][0]["competitors"][1]["score"]
         game_dict["away_score"] = away_score
@@ -248,20 +267,42 @@ def set_ncaaf_dict():
 
         ncaaf_dict[game_name] = game_dict
 
+# One time calls to set each team's id and color value for ticker
 
+set_ncaaf_teams_dict()
+
+# Repetitive calls to update score/time/other game info
 
 # set_nba_dict()
 # set_nfl_dict()
 # set_mlb_dict()
 set_ncaaf_dict()
-# # print(nba_dict)
-# # print(nfl_dict)
-# # print(mlb_dict)
-print(ncaaf_dict)
+
+#Print Statements to check output
+
+# print(ncaaf_teams_dict)
+# print(nba_dict)
+# print(nfl_dict)
+# print(mlb_dict)
+# print(ncaaf_dict)
+
+
+# Example loop for indexing the ncaaf_teams_dict by the id of each team in the ncaaf_dict
+# for game_name, game in ncaaf_dict.items():
+#     home_id = game["home_id"]
+#     away_id = game["away_id"]
+
+#     home_info = ncaaf_teams_dict.get(home_id, {})
+#     away_info = ncaaf_teams_dict.get(away_id, {})
+
+#     print(
+#         f"{game_name} | "
+#         f"HOME {home_id} - {home_info.get('conf')} - {home_info.get('color')} | "
+#         f"AWAY {away_id} - {away_info.get('conf')} - {away_info.get('color')}"
+#     )
 
 
 #                                                  FEATURES TO ADD
 
-# Change it so that NCAAF team info (color and conference) is only fetched once on start up and is stored in a separate dict
 # 
 # Add a dict for NFL and NBA teams so that it grabs their color and conference/division similar to NCAAF
